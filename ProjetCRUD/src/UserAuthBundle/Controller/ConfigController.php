9c7272f163c603944942299a7addc86d1383c0c1
Stan <sthot67@gmail.com>
2016-11-14 13:45:57 +0100

    Mode configuration de la base de données :
    - Ajout d'un formulaire pour modifier les conf bdd
    - Gestion des accès via roles user
    - Création d'une page d'erreur 503

--- ProjetCRUD/src/UserAuthBundle/Controller/ConfigController.php
+++ ProjetCRUD/src/UserAuthBundle/Controller/ConfigController.php
@@ -4,6 +4,7 @@ namespace UserAuthBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Dumper;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -20,6 +21,24 @@ class ConfigController extends Controller
 {
     public function indexAction(Request $request)
     {
+        $session = $request->getSession();
+        if($session->get('userRole') != 'admin')
+            return $this->render("UserAuthBundle:Error:503.html.twig");
+        if($request->isMethod("POST"))
+        {
+            $yaml = Yaml::parse(file_get_contents($this->container->get('kernel')->getRootDir() .'/config/parameters.yml'));
+
+            $yaml['parameters']['database_host'] = $request->get("host");
+            $yaml['parameters']['database_port'] = $request->get("port");
+            $yaml['parameters']['database_name'] = $request->get("name");
+            $yaml['parameters']['database_user'] = $request->get("user");
+            $yaml['parameters']['database_password'] = $request->get("password");
+
+            $new_yaml = Yaml::dump($yaml, 5);
+            file_put_contents($this->container->get('kernel')->getRootDir() .'/config/parameters.yml', $new_yaml);
+
+            return $this->render('UserAuthBundle:Home:index.html.twig');
+        }
         $datas = array();
 
         $datas['host'] = $this->container->getParameter('database_host');
@@ -27,22 +46,6 @@ class ConfigController extends Controller
         $datas['name'] = $this->container->getParameter('database_name');
         $datas['user'] = $this->container->getParameter('database_user');
         $datas['password'] = $this->container->getParameter('database_password');
-
-        if($request->isMethod("POST"))
-        {
-            //$this->container->setParameter('database_host',$request->get("host"));
-            $ymlDump = array(
-                'parameters' => array(
-                    'database_host' => $request->get("host")
-                ),
-            );
-
-            $dumper = new Dumper();
-
-            $yaml = $dumper->dump($ymlDump);
-            $path = __DIR__ . '/../../../app/config/parameters.yml';
-            file_put_contents($path, $yaml);
-        }
 
         return $this->render('UserAuthBundle:Config:index.html.twig',
             array(
